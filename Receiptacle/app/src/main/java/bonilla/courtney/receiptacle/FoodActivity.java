f459ccbd82090dca7beb7bf75fb84a7498eeb5fd
bonil100 <bonil100@mail.chapman.edu>
2017-12-12 19:53:29 -0800

    kind of has the take picture from camera working

--- Receiptacle/app/src/main/java/bonilla/courtney/receiptacle/FoodActivity.java
+++ Receiptacle/app/src/main/java/bonilla/courtney/receiptacle/FoodActivity.java
@@ -1,5 +1,7 @@
 package bonilla.courtney.receiptacle;
 
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -28,17 +30,26 @@ public class FoodActivity extends AppCompatActivity {
     }
 
 
-            public void takeImageFromCamera(View view) {
-                Intent cameraIntent = new Intent(Intent.ACTION_PICK);
-                cameraIntent.setType("image/*");
-                startActivityForResult(cameraIntent, GALLERY_REQUEST);
-            }
+    public void takeImageFromCamera(View view) {
+        Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        //cameraIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        cameraIntent.setType("image/*");
+        startActivityForResult(cameraIntent, GALLERY_REQUEST);
+    }
 
-            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-                if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
-                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-                    image.setImageBitmap(photo);
-                }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            final Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap photo = extras.getParcelable("data");
+                image.setImageBitmap(photo);
             }
+            //Bitmap photo = (Bitmap) data.getExtras().get("data");
+            //image.setImageBitmap(photo);
 
+
+        }
+
+    }
 }
